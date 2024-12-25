a5bb4a515658cacafbb427177c8b9ec20e4255d5
lu2000luk <lu2000luk@gmail.com>
2024-12-25 09:18:10 +0100

    Enhance AI interface with blur transition effect and improved shadow styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import Cap from '$lib/Cap.png';
-    import { fade } from 'svelte/transition';
+    import { fade, blur } from 'svelte/transition';
 
     let question = $state("");
     let content = $state("");
@@ -34,17 +34,17 @@
                 </button>
             </div>
         {:else}                
-            <div class="app-ai-bar flex items-center justify-between rounded-full w-full bg-[#27272a] mb-6 transition-all duration-200 hover:shadow-blue-600 hover:shadow-sm" transition:fade>
+            <div class="app-ai-bar flex items-center justify-between rounded-full w-full bg-[#27272a] mb-6 transition-all duration-200 shadow-blue-600 {question.length > 1 ? "shadow-sm" : "shadow-none"}" transition:fade>
                 <input bind:value={question} type="text" id="app-ai-input" class="ml-1 bg-transparent source border-none outline-none focus:ring-0 w-full my-1.5" placeholder="Ask me anything...">
                 {#if question.length > 1} 
-                    <div class="go-arrow p-2 rounded-full hover:bg-[#18181bab] cursor-pointer transition-all duration-200 mr-0.5">
+                    <div transition:blur class="go-arrow p-2 rounded-full hover:bg-[#18181bab] cursor-pointer transition-all duration-200 mr-0.5">
                         <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><path stroke-dasharray="20" stroke-dashoffset="20" d="M3 12h17.5"><animate fill="freeze" attributeName="stroke-dashoffset" dur="0.2s" values="20;0"/></path><path stroke-dasharray="12" stroke-dashoffset="12" d="M21 12l-7 7M21 12l-7 -7"><animate fill="freeze" attributeName="stroke-dashoffset" begin="0.2s" dur="0.2s" values="12;0"/></path></g></svg>
                     </div>
                 {/if}
             </div>
 
             <!-- svelte-ignore a11y_click_events_have_key_events -->
-            <div class="legal flex justify-center items-center gap-4 cursor-pointer mb-5" transition:fade>
+            <div class="legal flex justify-center items-center gap-4 cursor-pointer mb-5 shadow-sm" transition:fade>
                 <!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
                 <p class="hover:underline" onclick={() => {
                     content = "what";
